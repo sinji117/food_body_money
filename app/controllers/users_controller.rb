587fa43009799734ee4e9d916bdf54ca49f7efa7
sinji imagawa <yui.ibu.rami@gmail.com>
2020-09-23 12:38:23 +0900

    カラムと余計なファイルの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,5 @@
 class UsersController < ApplicationController
   def index
-    #@food = Food.all
     @food = current_user.foods
-  end
-
-  def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    #@food = Food.find(params[:food_id])
-    #@food = user.foods
-    @food = Food.all
-
-    #@food = user.foods.where("created_at >= ?", Date.today)
-    #@food = Food.where("created_at >= ?", Date.today)本日
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_073625) do
+ActiveRecord::Schema.define(version: 2020_09_23_031216) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 2020_09_22_073625) do
     t.integer "age", null: false
     t.integer "metabolism"
     t.integer "money"
-    t.datetime "start_time"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
