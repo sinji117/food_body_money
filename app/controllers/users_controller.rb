4b9ab4e5e91f5d2799a1392f8992cb0a3d83969a
sinji imagawa <yui.ibu.rami@gmail.com>
2020-09-23 11:43:09 +0900

    トップ画像の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    @food = Food.all
-  end
-
-  def new
-    @user = User.new
+    #@food = Food.all
+    @food = current_user.foods
   end
 
   def show
@@ -18,29 +14,4 @@ class UsersController < ApplicationController
     #@food = user.foods.where("created_at >= ?", Date.today)
     #@food = Food.where("created_at >= ?", Date.today)本日
   end
-
-  def create
-    User.create(user_parameter)
-    redirect_to users_path
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to users_path, notice:"削除しました"
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_parameter)
-      redirect_to users_path, notice: "編集しました"
-    else
-      render 'edit'
-    end
-  end
-
 end
