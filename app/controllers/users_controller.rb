02297b245d4034fb756d6eb9264f5536d407b455
sinji imagawa <yui.ibu.rami@gmail.com>
2020-09-26 15:35:04 +0900

    一週間チャレンジの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def index
     @food = current_user.foods
   end
+
+  def show
+    @food = current_user.foods
+  end
+
+  def edit
+    @food = current_user.foods
+  end
 end
